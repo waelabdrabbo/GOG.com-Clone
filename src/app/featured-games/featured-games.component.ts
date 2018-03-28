5af83d9fff909590eed724883b615038abe28d56
Wael Abdrabbo <waeel.us@gmail.com>
2018-03-28 18:24:44 +0200

    rebuild the grid system

--- src/app/featured-games/featured-games.component.ts
+++ src/app/featured-games/featured-games.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturedGamesComponent implements OnInit {
 
+  products = [];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  cartCount: number = 3;
   constructor() { }
 
   ngOnInit() {
